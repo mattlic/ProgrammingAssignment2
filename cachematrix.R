3548012a702feb5d8362c1582027b288adf66714
Matt Licholai <mattlic@outlook.com>
2014-04-25 18:40:18 -0700

    completed assignemnt and added test

--- cachematrix.R
+++ cachematrix.R
@@ -4,15 +4,15 @@
 ## makeCacheMatrix: This function creates a special "matrix" object 
 ##   that can cache its inverse.
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(xMatrixX = matrix()) {
     
     inverseMatrix <- NULL
     set <- function(y) {
-        x <<- y
+        xMatrixX <<- y
         inverseMatrix <<- NULL
     }
-    get <- function() x
-    setInverse <- function(solve) inverseMatrix <<- solve
+    get <- function() xMatrixX
+    setInverse <- function(inverse) inverseMatrix <<- inverse
     getInverse <- function() inverseMatrix
     list(set = set, get = get,
          setInverse = setInverse,
@@ -35,9 +35,52 @@ cacheSolve <- function(x, ...) {
         return(inverseMatrix)
     }
     data <- x$get()
-    getInverse <- solve(data, ...)
-    x$getInverse(inverseMatrix)
-    inverseMatrix
+    matrixInverse <- solve(data, ...)
+    x$setInverse(matrixInverse)
+    matrixInverse    
+}
+
+
+testCacheInverse <- function() {
+    hilbert <- function(n) { i <- 1:n; 1 / outer(i - 1, i, "+") }
+    h8 <- hilbert(8); h8
+    sh8 = solve(h8)
     
+    ch8 <- makeCacheMatrix( h8 )
+    sch8 <- cacheSolve( ch8 )
+    
+    if (sum(round(sh8 %*% h8, 3) == round(sch8 %*% h8)) == length(h8)) {
+        print("Simple test 1 .. OK" )
+    }
+    else {
+        Print("FAILED: Simple test 1 !!!")
+    }
+    
+    print(" ")
+    print("should see message ... 'getting cached data'")
+    if ( sum(round(sh8 %*% h8, 3) == round(cacheSolve( ch8 ) %*% h8)) == length(h8)) {
+        print("Simple test 2 .. OK")
+    }
+    else{
+        Print("FAILED: Simple test 2 !!!")
+    }
+    
+    print(" ")
+    print("should see message ... 'getting cached data'")
+    if ( sum(round(sh8 %*% h8, 3) == round(cacheSolve( ch8 ) %*% ch8$get())) == length(h8)) {
+        print("Simple test 3 .. OK")
+    }
+    else{
+        Print("FAILED: Simple test 3 !!!")
+    }
+    
+    print(" ")
+    print("using cache, but should not see a message ... ")
+    if ( sum(round(sh8 %*% h8, 3) == round( ch8$getInverse() %*% ch8$get())) == length(h8)) {
+        print("Simple test 4 .. OK")
+    }
+    else{
+        Print("FAILED: Simple test 4 !!!")
+    }
     
 }
